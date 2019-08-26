763d8f85321e2d5f6adb4cd88cb7c6e3e694d8b2
Eli Smaga <eli@confluent.io>
2019-08-26 15:25:51 -0700

    fix docker python api call

--- control-center/test/utils.py
+++ control-center/test/utils.py
@@ -23,7 +23,7 @@ def build_image(image_name, dockerfile_dir):
 
 def image_exists(image_name):
     client = docker.from_env(assert_hostname=False)
-    tags = [t for image in client.images() for t in image.tags()]
+    tags = [t for image in client.images().list() for t in image.tags()]
     return "{}:{}".format(image_name, "latest") in tags
 
 
